5b140aa60cce339b9f2c7d44b8c213c6d28d8e21
Mazaxaka_sf <arty_h41@mail.ru>
2023-11-29 18:32:33 +0300

    try  except

--- main.py
+++ main.py
@@ -16,16 +16,33 @@ with webdriver.Chrome() as browser:
     browser.get('https://web.chat2desk.com/chat/my?dialogID')
 
     # res = browser.get_cookies()
-    time.sleep(5)
+    time.sleep(10)
     # browser.find_element(By.ID, 'macros-icon-wrapper').click()
     # time.sleep(5)
     # browser.find_element(By.XPATH, '//div[@id="macroses-drop-down"]/div[3]')
     chat_items = browser.find_elements(By.CLASS_NAME, 'left-messages-container')
 
-    for i in chat_items:
-        i.click()
-        time.sleep(2)
+    c = 0
+    def f(chat, c = 0):
 
+        for i in chat:
+            try:
+                i.click()
+                time.sleep(1)
+                browser.find_element(By.ID, 'macros-icon-wrapper').click()
+                time.sleep(1)
+                browser.find_element(By.ID, 'macros-icon-wrapper').click()
 
+                time.sleep(5)
+                c += 1
+                if c == 5:
+                    browser.get('https://web.chat2desk.com/chat/my?dialogID')
+                    time.sleep(5)
+                    chat_items = browser.find_elements(By.CLASS_NAME, 'left-messages-container')
+                    return f(chat_items, c = 0)
+            except:
+                time.sleep(1)
+                print('не удачная попытка')
+                continue
 
-
+    f(chat_items)
